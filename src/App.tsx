3f843912f0961bba468526524868a68ae4e2c9ae
zhasmina <jasminaisaeva2001@gmail.com>
2023-05-07 23:18:20 +0600

    added switcher component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Swithcher } from "./components/UI/Swithcher";
+
 export const App = (): JSX.Element => {
   return (
     <div>
       <h1>My App</h1>
+      <Swithcher />
     </div>
   );
 };
