c72887d11580fe8f5d5b5fb568440f19f3efd561
zhasmina <jasminaisaeva2001@gmail.com>
2023-05-10 16:04:35 +0600

    fixed conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Checkboxx } from "./components/UI/Checkbox";
-
 export const App = (): JSX.Element => {
   return (
     <div>
       <h1>My App</h1>
-      <Checkboxx />
     </div>
   );
 };
