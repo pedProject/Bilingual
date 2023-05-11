7e8966e96d494973afb7b99e5ee886a649b06b57
Alisher <zumanovaliser814@gmail.com>
2023-05-11 16:48:18 +0600

    added dropdown

---
+++ src/components/UI/dropdown/Dropdown.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+import { Menu } from "@mui/material";
+
+// interface IMenuProps {
+//   children: ReactNode;
+// }
+
+// const Dropdown = ({ children }: IMenuProps): JSX.Element => {
+//   return <Menu>{children}</Menu>;
+// };
+
+// export default Dropdown;
