708e4b72227883d5395e2d7ca33956f622aabdcf
Elizar Aitbek <elizar.aitbek@gmail.com>
2023-05-12 10:01:21 +0600

    change svg file name

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,4 +2,4 @@ import { ReactComponent as CheckedIcon } from "./icons/checked.svg";
 import { ReactComponent as UnCheckedIcon } from "./icons/unchecked.svg";
 export { UnCheckedIcon, CheckedIcon };
 
-export { ReactComponent as CloseIcon } from "./icons/closeIcon.svg";
+export { ReactComponent as CloseIcon } from "./icons/close-icon.svg";
