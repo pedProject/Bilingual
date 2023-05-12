dad3ace6f56c6384f814c9f1e81171b94f45afa7
Elizar Aitbek <elizar.aitbek@gmail.com>
2023-05-12 09:54:42 +0600

    add initial styles, closeIcon prop, types

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 import { ReactComponent as CheckedIcon } from "./icons/checked.svg";
 import { ReactComponent as UnCheckedIcon } from "./icons/unchecked.svg";
 export { UnCheckedIcon, CheckedIcon };
+
+export { ReactComponent as CloseIcon } from "./icons/closeIcon.svg";
--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,13 +1,37 @@
-import { Dialog } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, IconButton, styled } from "@mui/material";
+
+import { CloseIcon } from "../../assets";
 
 import type { ModalProps } from "@mui/material";
 
-type Props = ModalProps;
+type Props = ModalProps & {
+  onClose: () => void;
+  closeIcon?: boolean;
+};
 
-export const Modal = ({ open, onClose, children }: Props): JSX.Element => {
+export const Modal = ({ open, onClose, children, closeIcon = false }: Props): JSX.Element => {
   return (
-    <Dialog open={open} onClose={onClose}>
-      {children}
-    </Dialog>
+    <StyledDialog open={open} onClose={onClose}>
+      {closeIcon ? (
+        <StyledDialogTitle>
+          <IconButton onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        </StyledDialogTitle>
+      ) : null}
+      <DialogContent>{children}</DialogContent>
+    </StyledDialog>
   );
 };
+
+const StyledDialog = styled(Dialog)(() => ({
+  "& .MuiPaper-root": {
+    borderRadius: "20px"
+  }
+}));
+
+const StyledDialogTitle = styled(DialogTitle)(() => ({
+  display: "flex",
+  justifyContent: "flex-end",
+  paddingTop: "20px"
+}));
