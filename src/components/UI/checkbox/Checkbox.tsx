a63aa2db9ac81906b910ce380a991a4f9f36c806
Alisher <zumanovaliser814@gmail.com>
2023-05-13 22:31:38 +0600

    fix your components

--- src/components/UI/checkbox/Checkbox.tsx
+++ src/components/UI/checkbox/Checkbox.tsx
@@ -3,17 +3,17 @@ import type { Ref } from "react";
 
 import { Checkbox as MuiCheckbox } from "@mui/material";
 
-import { CheckedIcon, UnCheckedIcon } from "./../../assets/index";
+import { CheckedIcon, UnCheckedIcon } from "../../../assets/index";
 
 import type { CheckboxProps } from "@mui/material";
 
-type Props = {
+interface ICheckboxProps {
   value: boolean | undefined;
   onChange: () => (event: React.ChangeEvent<HTMLInputElement>) => void;
-};
+}
 export const Checkbox = forwardRef(
   (
-    { value, onChange, ...props }: Props & CheckboxProps,
+    { value, onChange, ...props }: ICheckboxProps & CheckboxProps,
     ref: Ref<HTMLInputElement>
   ): JSX.Element => {
     return (
