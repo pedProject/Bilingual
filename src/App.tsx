ad42338fd0b2e8661ff3b86d3554ae9a42116ed1
Bektur12 <kanybekovbektur22@gmail.com>
2023-05-14 21:17:44 +0600

    finished yess

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import { Snackbar } from "./components/UI/Snackbar";
-import { useSnackbar } from "./hooks/useSnackbar";
-
 export const App = (): JSX.Element => {
-  const { notify } = useSnackbar();
-
-  notify({
-    type: "error",
-    title: "Authentication",
-    message: "Successfully sign in",
-    options: {
-      position: "bottom-left"
-    }
-  });
-
   return (
     <div>
       <h1>MyApp</h1>
-      <Snackbar />
     </div>
   );
 };
