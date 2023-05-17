2a26b28dbbfb8ed259b54fd50277d0de1045bfbc
Bektur12 <kanybekovbektur22@gmail.com>
2023-05-17 15:27:18 +0600

    add accordions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Accordions } from "./components/UI/Accordions";
-
 export const App = (): JSX.Element => {
   return (
     <div>
       <h1>MyApp</h1>
-      <Accordions />
     </div>
   );
 };
--- src/components/UI/Accordions.tsx
+++ src/components/UI/Accordions.tsx
@@ -5,6 +5,12 @@ import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 
+import { ReactComponent as Logo } from "../../assets/icons/Bilingual.svg";
+import { ReactComponent as AddIcon } from "../../assets/icons/addIcon.svg";
+import { ReactComponent as FacebookIcon } from "../../assets/icons/facebook.svg";
+import { ReactComponent as InstaIcon } from "../../assets/icons/instagram.svg";
+import { ReactComponent as YoutubeIcon } from "../../assets/icons/youtube.svg";
+
 import type { StyledComponent } from "@emotion/styled";
 import type { AccordionProps } from "@mui/material/Accordion";
 
@@ -41,18 +47,15 @@ export const Accordions = () => {
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     }
   ];
+
   return (
     <StyledFooter>
       <StyledContainer>
         <Header>FAQ:</Header>
         {Footerdata.map((el) => {
           return (
-            <StyledAccordion
-              key={el.panel}
-              //   expanded={expanded === el.panel}
-              //   onChange={handleChange(el.panel)}
-            >
-              <StyledAccordionSummary expandIcon={<div>beka</div>}>
+            <StyledAccordion key={el.panel}>
+              <StyledAccordionSummary aria-controls="panel1bh-content" expandIcon={<AddIcon />}>
                 <Typography>{el.question}</Typography>
               </StyledAccordionSummary>
               <StyledAccordionDetails>
@@ -62,44 +65,37 @@ export const Accordions = () => {
           );
         })}
       </StyledContainer>
-      <StyledFooterInline />
+      <StyledFooterInline>
+        <Logo />
+        <IconContainer>
+          <FacebookIcon />
+          <YoutubeIcon />
+          <InstaIcon />
+        </IconContainer>
+      </StyledFooterInline>
+      <Text>© Copyright PeakSoft. All Rights Reserved</Text>
     </StyledFooter>
   );
 };
 const StyledFooter = styled("div")`
-  width: 100%;
   background-color: #262626;
 `;
 const StyledFooterInline = styled("div")`
-  width: 1150px;
   height: 140px;
-  margin: auto;
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  img {
-    :nth-child(3) {
-      margin-left: 300px;
-    }
-  }
+  display: grid;
+  grid-template-columns: 37% 8%;
+  gap: 30%;
+  justify-content: center;
 `;
-// const StyledImage = styled("img")`
-//   width: 225px;
-//   height: 46px;
-// `;
-// const StyledText = styled("span")`
-//   margin-top: 110px;
-//   margin-left: 200px;
-//   font-family: Poppins;
-//   font-style: Regular;
-//   font-size: 14px;
-//   color: #98a2b3;
-// `;
-const StyledAccordionSummary = styled((props) => (
-  <AccordionSummary expandIcon={<div>hello</div>} {...props} />
-))(() => ({
+
+const Text = styled("h3")`
+  font-family: Poppins;
+  font-size: 14px;
+  color: #98a2b3;
+  text-align: center;
+`;
+const StyledAccordionSummary = styled(AccordionSummary)(() => ({
   background: "#262626",
-  flexDirection: "row",
   borderTop: "1px solid #4A4A4A",
 
   "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
@@ -116,7 +112,6 @@ const StyledAccordionSummary = styled((props) => (
     lineHeight: "40px"
   }
 }));
-
 const StyledAccordion: StyledComponent<AccordionProps & { children?: React.ReactNode }> = styled(
   (props: AccordionProps & { children?: React.ReactNode }) => <Accordion {...props} />
 )(() => ({
@@ -128,19 +123,21 @@ const StyledAccordion: StyledComponent<AccordionProps & { children?: React.React
   }
 }));
 
-const StyledAccordionDetails = styled(AccordionDetails)(() => ({}));
+const StyledAccordionDetails = styled(AccordionDetails)(() => ({
+  "&.MuiAccordionDetails-root": {
+    paddingLeft: "29px"
+  }
+}));
 
 const Header = styled("h1")`
   color: #ffffff;
   font-family: "Gilroy";
   font-weight: 700;
   font-size: 40px;
-  line-height: 51px;
-  margin-bottom: 10px;
+  margin-bottom: 30px;
 `;
 
 const StyledContainer = styled(Container)`
-  width: 1220px;
   padding: 120px 0;
   background-color: #262626;
 `;
@@ -154,4 +151,9 @@ const StyledTypographyAccordion = styled(Typography)`
   color: #ffffff;
   border-radius: none;
   margin-left: -16px;
+`;
+
+const IconContainer = styled("div")`
+  display: flex;
+  gap: 16px;
 `;
