2d3210ed9ab560273a7ed25a3fbcf8b6da1025ce
zhasmina <jasminaisaeva2001@gmail.com>
2023-05-18 18:36:08 +0600

    remove Slider component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import Slider from "./landing/components/slider/Slider";
-
 export const App = (): JSX.Element => {
   return (
     <div>
       <h1>MyApp</h1>
-      <Slider />
     </div>
   );
 };
