861549f6baacf133105c20e53abdb5ba4092f5e6
Kamchybekk <manasbekoovich@gmail.com>
2023-05-22 15:32:36 +0600

    fixed

--- src/components/UI/Checkbox.tsx
+++ src/components/UI/Checkbox.tsx
@@ -8,16 +8,7 @@ import { CheckedIcon, UnCheckedIcon } from "./../../assets/index";
 import type { CheckboxProps } from "@mui/material";
 
 export const Checkbox = forwardRef(
-<<<<<<< HEAD
   ({ value, onChange, ...props }: CheckboxProps, ref: Ref<HTMLInputElement>): JSX.Element => {
-=======
-
-  (
-    { value, onChange, ...props }: CheckboxProps & { value: boolean },
-    ref: Ref<HTMLInputElement>
-  ): JSX.Element => {
-
->>>>>>> bc88fbe02412bc6157ba9fc09816107cab46258e
     return (
       <MuiCheckbox
         {...props}
