554add4304de2a224685d2b208157a6f66cabf7f
Elizar Aitbek <elizar.aitbek@gmail.com>
2023-05-23 17:01:51 +0600

    save

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,41 @@ export enum ROLES {
   CLIENT = "CLIENT",
   ADMIN = "ADMIN"
 }
+
+export const ADVANTAGES_DATA = [
+  {
+    id: 1,
+    title: "Expand your applicant pool",
+    description:
+      "Tap into a diverse pool of candidates from 210+ countries and territories of origin, who have taken the Bilingual English Test because of its radical accessibility.",
+    image: ""
+  },
+  {
+    id: 2,
+    title: "Built on the latest assessment   sciencee",
+    description:
+      "The Duolingo English Test is a computer adaptive test backed by rigorous research, with results that are highly correlated with other major assessments such as the TOEFL and the IELTS.",
+    image: ""
+  },
+  {
+    id: 3,
+    title: "Innovative test security",
+    description:
+      "Industry-leading security protocols, individual test proctoring, and computer adaptive technology help prevent fraud and cheating and ensure results you can trust.",
+    image: ""
+  },
+  {
+    id: 4,
+    title: "Convenient results dashboard",
+    description:
+      "Access candidates certificates, video interviews, and writing samples through a free and secure dashboard. Quickly and easily view applicant data with filtering tools.",
+    image: ""
+  },
+  {
+    id: 5,
+    title: "Secure Design",
+    description:
+      "Adaptive test engine dynamically administers test questions from a database of hundreds of thousands of items. Someone would have to take the test more than 1,000 times to see a question repeated.",
+    image: ""
+  }
+];
