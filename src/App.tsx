5eda4fafc5ae05aa84923303f6a5706222716199
zhasmina <jasminaisaeva2001@gmail.com>
2023-05-31 12:23:17 +0600

    fixed video component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { LandingPage } from "./containers/Landing.Page";
+
 export const App = (): JSX.Element => {
   return (
     <>
-      <h1>MyApp</h1>
+      {/* <h1>MyApp</h1> */}
+      <LandingPage />
     </>
   );
 };
