7e15c04ca72231415f91b7a6d0de707e7135b45e
zhasmina <jasminaisaeva2001@gmail.com>
2023-05-31 12:41:30 +0600

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { LandingPage } from "./containers/Landing.Page";
-
 export const App = (): JSX.Element => {
-  return (
-    <>
-      {/* <h1>MyApp</h1> */}
-      <LandingPage />
-    </>
-  );
+  return <>{<h1>MyApp</h1>}</>;
 };
