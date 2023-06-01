f6ea27fd86188f6087449bbcc1c2c9ad92e81663
Alisher <zumanovaliser814@gmail.com>
2023-06-01 14:03:39 +0600

    create custom hook useTimer and fixed commits

--- src/components/UI/timer-progress-bar/TimerProgressBar.tsx
+++ src/components/UI/timer-progress-bar/TimerProgressBar.tsx
@@ -1,48 +1,34 @@
-import { useEffect, useState } from "react";
-
-import { styled } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import { Line } from "rc-progress";
 import { type ProgressProps } from "rc-progress";
 
-interface Props extends ProgressProps {
+import { useTimer } from "../../../hooks/useTimer";
+
+export interface ProgressBarProps {
   duration: number;
+  onTimeUp: () => void;
 }
 
-export const TimerProgressBar = ({ duration, ...props }: Props) => {
-  const [remainingTime, setRemainingTime] = useState(duration);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setRemainingTime((prevTime) => {
-        if (prevTime === 0) {
-          clearInterval(timer);
-          return 0;
-        }
-        return prevTime - 1;
-      });
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  const progress = (duration - remainingTime) / duration;
-  const minutes = Math.floor(remainingTime / 60);
-  const seconds = remainingTime % 60;
-  const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+export const TimerProgressBar = ({
+  onTimeUp,
+  duration = 0,
+  ...props
+}: ProgressBarProps & ProgressProps) => {
+  const { formattedTime, percent } = useTimer({ duration, onTimeUp });
 
   return (
-    <>
-      <StyledTimer>{formattedTime}</StyledTimer>
-      <Line percent={progress * 100} strokeColor="#3A10E5" trailColor="#D4D0D0" {...props} />
-    </>
+    <StyledBox>
+      <p>{formattedTime}</p>
+      <Line percent={percent} strokeColor="#3A10E5" trailColor="#D4D0D0" {...props} />
+    </StyledBox>
   );
 };
 
-const StyledTimer = styled("p")(() => ({
-  paddingBottom: "20px",
-  fontWeight: "600",
-  fontSize: "2rem",
-  color: "#4C4859"
+const StyledBox = styled(Box)(() => ({
+  "& > p": {
+    marginBottom: "20px",
+    fontWeight: "600",
+    fontSize: "2rem",
+    color: "#4C4859"
+  }
 }));
